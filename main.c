fb1deb0f7a38caf7b07c30aec039ccfec09aca52
Sebastián Bedoya <sebastianbedoya25@gmail.com>
2024-09-27 21:32:13 -0500

    Se crean los métodos validarArchivosInvertidos y validarCantidadArgumentos

--- main.c
+++ main.c
@@ -45,3 +45,59 @@ void validarContenidoDiffArchivo(const char *nombreArch1,const char *nombreArch2
     }
 
 } 
+
+int validarArchivosInvertidos(const char *nombreArch1,const char *nombreArch2){
+    
+    FILE *in = fopen(nombreArch1,"r");; 
+    FILE *out = fopen(nombreArch2,"w");
+
+  	//Arreglo de punteros a caracteres
+  	char **lines = NULL;
+
+  	//Contador para el numero de lineas
+  	int lineCount = 0;
+
+  	// puntero 'line' para almacenar dinámicamente cada línea leída
+	// 'len' para rastrear la longitud de la línea actual.
+  	char *line = NULL;
+  	size_t len = 0;
+
+  	while (getline(&line, &len, in ) != -1){
+
+  		lineCount++;
+		
+		// Redimensionar dinámicamente el arreglo 'lines' para acomodar la nueva línea.
+		lines = realloc(lines, lineCount * sizeof(char *));
+
+		// Verificar si la asignación de memoria fue exitosa.
+		if (lines == NULL) {
+        fprintf(stderr, "reverse: error allocating memory\n");
+        exit(ERROR);
+    	}
+
+    	// Duplicar la línea recién leída y almacenar su dirección en el arreglo 'lines'.
+		lines[lineCount - 1] = strdup(line);
+
+		// Verificar si la duplicación de la línea fue exitosa.
+		if (lines[lineCount - 1] == NULL) {
+        fprintf(stderr, "reverse: error duplicating line\n");
+        exit(ERROR);
+    	}
+  	}
+
+  	free(line);
+
+  	//imprimir arreglo desde su ultima posicion a la inicial
+  	for (int i = lineCount - 1; i >= 0; i--) {
+  		fprintf(out,"%s",lines[i]);
+  		free(lines[i]);
+  	}
+    
+  	free(lines);
+
+  	//Cerrar archivos
+  	fclose(in);
+  	fclose(out);
+
+    return 0;
+}
